aae7b60798d4fcddc5fa33c5d268616d83412f08
franc333s <marinafase@gmail.com>
2023-04-20 20:54:44 +0200

    Practice mixins import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import './scss/style.scss'
+// Scss metido aquí en vez de en la raíz
 import Home from './views/Home'
 import Contact from './views/Contact'
+import Topbar from './components/topbar/Topbar'
 
 function App() {
   return (
     <>
       <header>
+        <Topbar />
         <h1>Project Gamma</h1>
 		<Link to="/">Home</Link>
 		<Link to="/contact">Contact</Link>
